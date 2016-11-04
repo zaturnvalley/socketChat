06413e4d5ae8c5fae42409a54536eaf83b8b9605
zaturnvalley <zacharycmason@gmail.com>
2016-11-04 15:02:19 -0700

    bs navbar via index.jade

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Index Route
 app.get('/', function(req,res){
-  res.send('hello');
+  res.render('index');
 });
 
 server.listen(process.env.PORT || 3000);
